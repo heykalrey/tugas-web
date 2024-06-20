e8b8d25c82b4981e11a72e5a6e629db44f55fbdc
unknown <you@example.com>
2024-06-20 22:47:29 +0700

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                   2024 © Alina
+                </footer>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 "style="background-color: purple;">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Alina 2024</p>
+      </div>
+      <!-- /.container -->
+    </footer>
